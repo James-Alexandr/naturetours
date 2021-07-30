5c892691808eeeb144d325fa09ca9489a5fc4c7a
Jalexander01 <60295314+Jalexander01@users.noreply.github.com>
2021-07-29 23:03:59 -0500

    update

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -2,6 +2,7 @@
 import './about.scss';
 import '../grid-layout/grid-layout.scss';
 import H2 from '../h2/h2.jsx';
+import '../../styles/global/utilities.scss'
 import React from 'react';
 
 
@@ -18,7 +19,22 @@ export default class About extends React.Component {
                 </div>
                 <div className="row">
                     <div className="col-1-of-2">
-                        Text
+                        <h3 className="heading-h3 u-margin-bottom-2">
+                            Do you ever fall in love with nature?
+                        </h3>
+                        <p className="paragraph">
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                            Quibusdam facere deserunt rerum officia ex molestiae tempora.
+                        </p>
+                        <h3 className="heading-h3  u-margin-bottom-2">
+                            Falling in Love With the Natural World.</h3>
+                        <p className="paragraph">
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                            Quibusdam facere deserunt rerum officia ex molestiae tempora.
+                        </p>
+                        <a href="#" className="btn-text">
+                            Learn more &rarr;
+                        </a>
                     </div>
                     <div className="col-1-of-2">
                         Img
