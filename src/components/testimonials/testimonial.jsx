ab696b48553ac00c51303670d45491f72c682ff8
Jalexander01 <60295314+Jalexander01@users.noreply.github.com>
2021-08-02 21:16:54 -0500

    update

--- src/components/testimonials/testimonial.jsx
+++ src/components/testimonials/testimonial.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import H2 from '../h2/h2.jsx';
 
-
+import image1 from '../../img/nat-8.jpg';
 
 
 export default class About extends React.Component {
@@ -14,7 +14,14 @@ export default class About extends React.Component {
                 </div>
                 <div className="row">
                     <div className="story">
-                        Some text
+                        <figure className="story__shape">
+                            <img src={image1} alt="Person on a tour" className="story__image" />
+                        </figure>
+                        <div className="story__text">
+                            <h3 className="heading-h3 u-margin-bottom-2 ">
+                                Best week of my life</h3>
+                            <p > Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt dolor itaque soluta. Nostrum incidunt minus illum nemo, earum obcaecati doloribus, a numquam sint possimus odio asperiores! Omnis doloribus aliquam mini</p>
+                        </div>
                     </div>
                 </div>
             </section>
