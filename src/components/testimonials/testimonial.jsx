8df569356a4fbc9e12be645ee1f3a6b52e3844f6
Jalexander01 <60295314+Jalexander01@users.noreply.github.com>
2021-08-03 09:54:01 -0500

    update

--- src/components/testimonials/testimonial.jsx
+++ src/components/testimonials/testimonial.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import H2 from '../h2/h2.jsx';
 
 import image1 from '../../img/nat-8.jpg';
+import image2 from '../../img/nat-9.jpg';
+import image3 from '../../img/nat-10.jpg';
 
 
 export default class About extends React.Component {
@@ -15,7 +17,9 @@ export default class About extends React.Component {
                 <div className="row">
                     <div className="story">
                         <figure className="story__shape">
+
                             <img src={image1} alt="Person on a tour" className="story__image" />
+                            <figcaption className="story__caption">Sonia Stevens</figcaption>
                         </figure>
                         <div className="story__text">
                             <h3 className="heading-h3 u-margin-bottom-2 ">
@@ -23,6 +27,38 @@ export default class About extends React.Component {
                             <p > Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt dolor itaque soluta. Nostrum incidunt minus illum nemo, earum obcaecati doloribus, a numquam sint possimus odio asperiores! Omnis doloribus aliquam mini</p>
                         </div>
                     </div>
+                </div>
+                <div className="row">
+                    <div className="story">
+                        <figure className="story__shape">
+
+                            <img src={image2} alt="Person on a tour" className="story__image" />
+                            <figcaption className="story__caption">Mario Orta</figcaption>
+                        </figure>
+                        <div className="story__text">
+                            <h3 className="heading-h3 u-margin-bottom-2 ">
+                                Had lots of fun</h3>
+                            <p > Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt dolor itaque soluta. Nostrum incidunt minus illum nemo, earum obcaecati doloribus, a numquam sint possimus odio asperiores! Omnis doloribus aliquam mini</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="story">
+                        <figure className="story__shape">
+
+                            <img src={image3} alt="Person on a tour" className="story__image" />
+                            <figcaption className="story__caption">Miriam Okle</figcaption>
+                        </figure>
+                        <div className="story__text">
+                            <h3 className="heading-h3 u-margin-bottom-2 ">
+                                Relaxing and exiting vacations</h3>
+                            <p > Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt dolor itaque soluta. Nostrum incidunt minus illum nemo, earum obcaecati doloribus, a numquam sint possimus odio asperiores! Omnis doloribus aliquam mini</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="u-center-text u-margin-top-huge" >
+                    <a href="#" className="btn btn-text ">Discover all tours &rarr;</a>
                 </div>
             </section>
 
