4b7a0d981976c6d2190b0969eca824e42de88b67
Jalexander01 <60295314+Jalexander01@users.noreply.github.com>
2021-08-04 10:19:20 -0500

    update

--- src/components/booking/booking.js
+++ src/components/booking/booking.js
@@ -29,6 +29,21 @@ export default class Booking extends React.Component {
                                     </input>
                                     <label htmlFor="text" className="form__label" id="name"> Email</label>
                                 </div>
+                                <div className="form_group">
+                                    <div className="form__radio-group">
+                                        <input type="radio" id="small" className="form__radio-input" name="size" />
+                                        <label for="small" className="form__radio-label">
+                                            <span className="form__radio-button"></span>
+                                            Small tour group</label>
+                                    </div>
+                                    <div className="form__radio-group">
+                                        <input type="radio" id="large" className="form__radio-input" name="size" />
+                                        <label for="large" className="form__radio-label">
+                                            <span className="form__radio-button"></span>
+
+                                            Large tour group</label>
+                                    </div>
+                                </div>
                             </form>
                         </div>
                     </div>
